22b7e206f6126169c84da33192b4bd3f54e04bb3
Afif Fahmi <afif.rafpi@gmail.com>
2023-10-10 04:03:19 +0800

    connection to mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,23 @@
 import express from "express";
-import { PORT } from "./config.js"
-
+import { PORT, mongoDBURL } from "./config.js"
+import mongoose from "mongoose";
 
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+app.get('/', (request,response) => {
+ console.log(request);
+ return response.status(200).send("Welcome to MERN STACK");
+});
+
+
+
+mongoose.connect(mongoDBURL).then(() => {
+    console.log('Connected to DB');
+    app.listen(PORT, () => {
+        console.log(`listening on port ${PORT}`);
+    });
+}).catch((error) => {
+    console.log(error);
 })
 
